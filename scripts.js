e412b0fad4902dbcf8a4f33c80164d0bc833ad46
LukaszWiszniewski <wisznu07@gmail.com>
2017-11-30 15:18:32 +0100

    initial

---
+++ scripts.js
@@ -0,0 +1,6 @@
+var a=10,
+    b=25,
+    value= (a * a) - (2 * a * b) + (b * b);
+if(value>0){console.log('number is over 0')}
+else if (value<0){console.log('number is below 0')}
+else{console.log('number equal 0')}
